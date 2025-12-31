853d5b6b7759164f94a062d3385a2c3b58bbeef1
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-12-31 11:09:43 -0800

    Initial Commit

---
+++ GlassVariants/GlassVariantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassVariantsApp.swift
+//  GlassVariants
+//
+//  Created by Morris Richman on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct GlassVariantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
