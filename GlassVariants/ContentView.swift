e65cd19f324fcfed00a3aa7ad7842758fc2e7c8f
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2026-01-15 12:16:04 -0800

    add glassview swiftui view

--- GlassVariants/ContentView.swift
+++ GlassVariants/ContentView.swift
@@ -7,13 +7,24 @@
 
 import SwiftUI
 
-struct GlassEffectView: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> UINavigationController {
-        .init(rootViewController: GlassEffectViewController())
-    }
-    
-    func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {
-        
+//struct GlassEffectView: UIViewControllerRepresentable {
+//    func makeUIViewController(context: Context) -> UINavigationController {
+//        .init(rootViewController: GlassEffectViewController())
+//    }
+//    
+//    func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {
+//        
+//    }
+//}
+
+struct GlassEffectView: View {
+    var body: some View {
+        GlassView(variant: 0)
+        .background {
+            Image(._0)
+                .resizable()
+                .scaledToFill()
+        }
     }
 }
 
