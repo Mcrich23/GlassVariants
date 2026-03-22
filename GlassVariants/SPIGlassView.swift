78704d46ee6a62be6bffc9e17be41e34f09b4108
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2026-03-22 13:52:42 -0700

    Update SPIGlassView.swift

--- GlassVariants/SPIGlassView.swift
+++ GlassVariants/SPIGlassView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI_LiquidGlass
 
+extension Material: @retroactive ShapeStyle {}
+
 extension ShapeStyle where Self == Material {
     public static func _glass(_ variant: Material._GlassVariant = .regular) -> Material {
         return Material._glass(variant)
